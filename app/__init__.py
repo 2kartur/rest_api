e9a204ab80818e848915ffdc6c37c23ad2520200
2kartur <yakubishinartur@gmail.com>
2021-06-11 14:36:08 +0300

    'HEROKU'

--- app/__init__.py
+++ app/__init__.py
@@ -9,14 +9,16 @@ db = SQLAlchemy()
 def create_app(config_filename=None):
     app = Flask(__name__, instance_relative_config=True)
     with app.app_context():
-        # if os.environ.get('DATABASE_URL') is None: # for local work
-        #     app.config.update(SQLALCHEMY_DATABASE_URI = 'sqlite:///form.db', SECRET_KEY = 'asfdsfsaaffdf')
-        # else: # for heroku work
-        #     app.config.from_object('config')
+        if os.environ.get('DATABASE_URL') is None: # for local work
+            app.config.update(SQLALCHEMY_DATABASE_URI = 'sqlite:///form.db', SECRET_KEY = 'sdfdfdsf')
+        else: # for heroku work
+            app.config.from_object('config')
         # print(os.environ.get('DATABASE_URL'))
+        # app.config.from_object('config')
         db.init_app(app)
 
         from .api import api_bp
+        from . import view
         #from .swagger import swagger_bp
 
         app.register_blueprint(api_bp, url_prefix='/api')
@@ -24,3 +26,4 @@ def create_app(config_filename=None):
 
 
     return app
+app = create_app()
--- app/view.py
+++ app/view.py
@@ -1,5 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from datetime import datetime, date, timezone, timedelta
+import sys
+import os
+from os import abort
 from flask import current_app as app
+# from app import app, bcrypt #  import current_app
+import pytz
 
 @app.route('/')
 def index():
-    return 'dasd'
+    return render_template('main.html')
